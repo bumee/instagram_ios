cb374730f7f296109a54fb4e00b423a8d64cb757
Bumee <favvuri@naver.com>
2023-02-14 14:42:23 +0900

    first-commit

---
+++ Instagram/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by kibum on 2023/02/14.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
